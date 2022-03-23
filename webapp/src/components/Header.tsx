74a5b527b82e6360ca958d78f6841664857d6683
Florian Glombik <ge25bas@mytum.de>
2022-03-23 09:46:03 +0100

    Adding header

---
+++ webapp/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import './Header.css'
+
+export const Header = () =>  {
+    return (
+        <header className="page-header">
+            <h1>Workplace Reservation</h1>
+        </header>
+    )
+}
