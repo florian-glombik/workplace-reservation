4963c7086530664310f36190f8352a42c0410cec
Florian Glombik <florian.glombik@tum.de>
2022-05-01 05:16:12 +0200

    Adding login screen

--- webapp/src/components/Login.tsx
+++ webapp/src/components/Login.tsx
@@ -2,19 +2,36 @@ import './Login.css'
 
 export const Login = () => {
   return (
-    <div>
-      <div className="form-group">
-        <label htmlFor="exampleInputEmail1">Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          placeholder="Enter email"
-        />
-        <small id="emailHelp" className="form-text text-muted">
-          We'll never share your email with anyone else.
-        </small>
+    <div className="d-flex justify-content-center">
+      <div style={{ maxWidth: '20rem' }}>
+        {' '}
+        <div className="form-group mt-3">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="form-group mt-3">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
+        </div>
+        <div className="d-flex justify-content-between mt-4">
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+          <button type="submit" className="btn btn-secondary">
+            Register
+          </button>
+        </div>
       </div>
     </div>
   )
