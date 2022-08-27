e4a9e705ec9ea20afc74554d3a3c15e1f2a56740
Florian Glombik <florian.glombik@selectcode.de>
2022-08-27 23:33:06 +0200

    Adding home link to header

--- webapp/src/components/Header.tsx
+++ webapp/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import './Header.css'
 import { useAuth } from '../utils/AuthProvider'
+import { Link, Typography } from '@material-ui/core'
 
 type HeaderProps = {
   showLogoutButton?: boolean
@@ -14,9 +15,14 @@ export const Header = (props: HeaderProps) => {
     logout()
   }
 
+  // TODO use AppBar instead
   return (
     <header className="page-header d-flex justify-content-between">
-      <h1>Workplace Reservation</h1>
+      <Typography variant={'h4'}>
+        <Link href={'/'} underline={'none'}>
+          Workplace Reservation
+        </Link>
+      </Typography>
       {showLogoutButton && (
         <button className={'btn btn-primary m-2'} onClick={handleLogout}>
           Logout
