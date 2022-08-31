5d3f30f4bc95b378153d577f658af7fd7115779f
Florian Glombik <florian.glombik@selectcode.de>
2022-08-31 03:07:22 +0200

    Using AppBar for header

--- webapp/src/components/Header.tsx
+++ webapp/src/components/Header.tsx
@@ -1,6 +1,19 @@
 import './Header.css'
 import { useAuth } from '../utils/AuthProvider'
 import { Box, Button, Link, Typography } from '@material-ui/core'
+import {
+  AppBar,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  Menu,
+  MenuItem,
+  Switch,
+  Toolbar,
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+import { AccountCircle } from '@mui/icons-material'
+import { useRef, useState } from 'react'
 
 type HeaderProps = {
   showLogoutButton?: boolean
@@ -8,36 +21,73 @@ type HeaderProps = {
 
 export const Header = (props: HeaderProps) => {
   //@ts-ignore
-  const { logout } = useAuth()
+  const { logout, user } = useAuth()
   const { showLogoutButton = true } = props
+  const [anchorEl, setAnchorEl] = useState(null)
 
-  const handleLogout = () => {
-    logout()
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // setAuth(event.target.checked);
   }
 
-  // TODO use AppBar instead
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    //@ts-ignore
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        bgcolor: 'text.disabled',
-      }}
-    >
-      <Box sx={{ m: 2 }}>
-        <Typography variant={'h4'}>
-          <Link href={'/'} underline={'none'}>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant={'h6'} component={'div'}>
             Workplace Manager
-          </Link>
-        </Typography>
-      </Box>
-      {showLogoutButton && (
-        <Box sx={{ m: 2 }}>
-          <Button variant={'contained'} onClick={handleLogout}>
-            Logout
-          </Button>
-        </Box>
-      )}
+          </Typography>
+          {!!user && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
     </Box>
   )
 }
