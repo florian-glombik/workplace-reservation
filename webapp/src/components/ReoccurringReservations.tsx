bf8393055594d4563f9392ff6ce8215da0c04224
Florian Glombik <florian.glombik@selectcode.de>
2022-09-01 21:00:03 +0200

    Fixing repetition interval selection

--- webapp/src/components/ReoccurringReservations.tsx
+++ webapp/src/components/ReoccurringReservations.tsx
@@ -12,11 +12,18 @@ import {
   Typography,
 } from '@mui/material'
 import { endOfWeek, format, startOfWeek } from 'date-fns'
-import { WEEK_STARTS_ON_MONDAY } from './WorkplaceAccordions'
+import { DAYS_PER_WEEK, WEEK_STARTS_ON_MONDAY } from './WorkplaceAccordions'
 import * as React from 'react'
 import { DateRange, DateRangePicker } from 'mui-daterange-picker'
 import { ACCORDION_LABEL_DATE_FORMAT } from './Workplaces'
 import AddIcon from '@mui/icons-material/Add'
+
+enum RepetitionInterval {
+  weekly = DAYS_PER_WEEK,
+  everySecondWeek = DAYS_PER_WEEK * 2,
+  everyThirdWeek = DAYS_PER_WEEK * 3,
+  monthly = DAYS_PER_WEEK * 4,
+}
 
 export const ReoccurringReservations = () => {
   // @ts-ignore
@@ -39,25 +46,25 @@ export const ReoccurringReservations = () => {
   // @ts-ignore
   const endOfTheWeek = endOfWeek(today, WEEK_STARTS_ON_MONDAY)
 
-  const [dayOfTheWeek, setDayOfTheWeek] = useState(undefined)
+  const [dayOfTheWeek, setDayOfTheWeek] = useState('monday')
+  const [repetitionInterval, setRepetitionInterval] = useState(
+    RepetitionInterval.weekly
+  )
   const [startOfReoccurringReservation, setStartOfReoccurringReservation] =
     useState(new Date())
   const [endOfReoccurringReservation, setEndOfReoccurringReservation] =
-    useState(null)
+    useState(undefined)
 
   const [selectedDateAsString, setSelectedDateAsString] = useState('')
 
-  const handleChange = (event: SelectChangeEvent) => {
+  const handleWeekdaySelection = (event: SelectChangeEvent) => {
     // @ts-ignore
     setDayOfTheWeek(event.target.value as string)
   }
 
-  const handleStartDateChange = (date: any) => {
-    setStartOfReoccurringReservation(date)
-  }
-
-  const handleEndDateChange = (date: any) => {
-    setEndOfReoccurringReservation(date)
+  const handleRepetitionSelection = (event: SelectChangeEvent) => {
+    // @ts-ignore
+    setRepetitionInterval(event.target.value as string)
   }
 
   const convertDateRangeToString = (dateRange: DateRange) => {
@@ -72,54 +79,80 @@ export const ReoccurringReservations = () => {
 
   return (
     <Box>
-      <Typography variant={'h4'} sx={{ mt: 2 }}>
+      <Typography variant={'h4'} sx={{ mt: 2, mb: 2 }}>
         Reoccurring Reservations
       </Typography>
       {/*<Box sx={{ display: 'flex', justifyContent: 'center' }}>*/}
       <Box>
-        <FormControl variant={'standard'} sx={{ display: 'flex' }}>
-          <Grid>
-            <InputLabel id="weekday-selection-label" sx={{ ml: 2 }}>
-              Weekday
-            </InputLabel>
-            <Select
-              labelId="weekday-selection-label"
-              value={dayOfTheWeek}
-              onChange={handleChange}
-              required
-              sx={{ m: 2, minWidth: '6rem' }}
-            >
-              <MenuItem value={'monday'}>Monday</MenuItem>
-              <MenuItem value={'tuesday'}>Tuesday</MenuItem>
-              <MenuItem value={'wednesday'}>Wednesday</MenuItem>
-              <MenuItem value={'friday'}>Friday</MenuItem>
-              <MenuItem value={'saturday'}>Saturday</MenuItem>
-              <MenuItem value={'sunday'}>Sunday</MenuItem>
-            </Select>
-            <Select></Select>
-
-            {/*TODO fix requirements*/}
-            {/*TODO next week etc instead of last*/}
-            <TextField
-              label={'Timespan'}
-              onClick={toggleDateSelection}
-              required
-              value={selectedDateAsString}
-            />
-            <IconButton type={'submit'}>
-              <AddIcon></AddIcon>
-            </IconButton>
-            <DateRangePicker
-              open={open}
-              toggle={toggleDateSelection}
-              onChange={(dateRange: DateRange) => {
-                setDateRange(dateRange)
-                setSelectedDateAsString(convertDateRangeToString(dateRange))
-              }}
-              minDate={today as unknown as Date}
-            />
-          </Grid>
+        <FormControl>
+          <InputLabel id="weekday-selection-label">Weekday</InputLabel>
+          <Select
+            labelId="weekday-selection-label"
+            value={dayOfTheWeek}
+            onChange={handleWeekdaySelection}
+            required
+            sx={{ m: 2, minWidth: '6rem' }}
+          >
+            <MenuItem value={'monday'}>Monday</MenuItem>
+            <MenuItem value={'tuesday'}>Tuesday</MenuItem>
+            <MenuItem value={'wednesday'}>Wednesday</MenuItem>
+            <MenuItem value={'friday'}>Friday</MenuItem>
+            <MenuItem value={'saturday'}>Saturday</MenuItem>
+            <MenuItem value={'sunday'}>Sunday</MenuItem>
+          </Select>
         </FormControl>
+
+        <FormControl>
+          <InputLabel id="repeat-interval-section">
+            Repetition interval
+          </InputLabel>
+          <Select
+            labelId="repeat-interval-section"
+            id={'repeat-interval-section'}
+            // @ts-ignore
+            value={repetitionInterval}
+            onChange={handleRepetitionSelection}
+            required
+            sx={{ m: 2, minWidth: '10rem' }}
+          >
+            <MenuItem value={RepetitionInterval.weekly}>weekly</MenuItem>
+            <MenuItem value={RepetitionInterval.everySecondWeek}>
+              every second week
+            </MenuItem>
+            <MenuItem value={RepetitionInterval.everyThirdWeek}>
+              every third week
+            </MenuItem>
+            <MenuItem value={RepetitionInterval.monthly}>every month</MenuItem>
+          </Select>
+        </FormControl>
+
+        {/*TODO fix requirements */}
+        {/*TODO next week etc instead of last */}
+        {/*TODO remove highlight on weekday selection */}
+
+        <FormControl>
+          <TextField
+            label={'Timespan'}
+            onClick={toggleDateSelection}
+            required
+            value={selectedDateAsString}
+          />
+        </FormControl>
+
+        <IconButton type={'submit'}>
+          <AddIcon></AddIcon>
+          <Typography>Add reservation</Typography>
+        </IconButton>
+
+        <DateRangePicker
+          open={open}
+          toggle={toggleDateSelection}
+          onChange={(dateRange: DateRange) => {
+            setDateRange(dateRange)
+            setSelectedDateAsString(convertDateRangeToString(dateRange))
+          }}
+          minDate={today as unknown as Date}
+        />
       </Box>
     </Box>
   )
