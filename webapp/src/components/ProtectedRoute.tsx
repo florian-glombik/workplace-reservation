5211768bb1eac2ac7bc80fb351539e67c86b1540
Florian Glombik <florian.glombik@selectcode.de>
2022-09-15 01:42:20 +0200

    Automatically logout user when loading page with expired token

--- webapp/src/components/ProtectedRoute.tsx
+++ webapp/src/components/ProtectedRoute.tsx
@@ -1,10 +1,41 @@
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../utils/AuthProvider'
+// @ts-ignore
+import jwt_decode from 'jwt-decode';
+import { isBefore } from 'date-fns'
+
+type JwtToken = {
+  id: string,
+  userId: string
+  issuedAt: Date,
+  expiredAt: Date,
+}
+
+function convertJwtToken(token: string): JwtToken | null {
+  let convertedToken: any
+  try {
+    convertedToken = jwt_decode(token)
+  } catch (error) {
+    return null
+  }
+
+  return {
+    id: convertedToken.id,
+    userId: convertedToken.userId,
+    issuedAt: new Date(convertedToken.issuedAt),
+    expiredAt: new Date(convertedToken.expiredAt)
+  }
+}
 
 export const ProtectedRoute = ({ children }: any) => {
   //@ts-ignore
-  const userIsAuthenticated = useAuth().user
+  const {user, jwtToken, logout} = useAuth()
+
+  const convertedToken: JwtToken | null = convertJwtToken(jwtToken)
+
+  const userIsAuthenticated = user && convertedToken && isBefore(Date.now(), convertedToken.expiredAt)
   if (!userIsAuthenticated) {
+    logout()
     return <Navigate to="/login" />
   }
   return children
