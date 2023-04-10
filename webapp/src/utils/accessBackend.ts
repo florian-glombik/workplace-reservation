9809a6ead240e20344f2b34c206e61c33df66dff
Florian Glombik <florian.glombik@tum.de>
2023-04-10 02:29:34 +0200

    fixing format

--- webapp/src/utils/accessBackend.ts
+++ webapp/src/utils/accessBackend.ts
@@ -1,6 +1,6 @@
 // const BACKEND_DOMAIN = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost'
-const BACKEND_BASE_URL = "http://localhost:8080/"
+const BACKEND_BASE_URL = 'http://172.17.0.1:8080'
 
 export function composeBackendUrl(path: string) {
-    return BACKEND_BASE_URL + (path.startsWith("/") ? path : `/${path}`)
+  return BACKEND_BASE_URL + (path.startsWith('/') ? path : `/${path}`)
 }
