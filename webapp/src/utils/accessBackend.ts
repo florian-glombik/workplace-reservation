0e77ba64993aff6565ced5b9b2bc76961f35e9b6
Florian Glombik <florian.glombik@tum.de>
2023-04-10 03:34:49 +0200

    Adding caddy to docker compose

--- webapp/src/utils/accessBackend.ts
+++ webapp/src/utils/accessBackend.ts
@@ -1,5 +1,5 @@
-// const BACKEND_DOMAIN = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost'
-const BACKEND_BASE_URL = 'http://172.17.0.1:8080'
+const BACKEND_BASE_URL =
+  process.env.REACT_APP_BACKEND_URL ?? 'http://172.17.0.1:8080'
 
 export function composeBackendUrl(path: string) {
   return BACKEND_BASE_URL + (path.startsWith('/') ? path : `/${path}`)
