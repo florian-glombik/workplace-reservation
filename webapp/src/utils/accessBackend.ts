ee749142fad9cb7f9e7db479d4983b5814fdf266
Florian Glombik <florian.glombik@tum.de>
2024-04-13 17:02:54 +0200

    Adding warning if server URL is not set

--- webapp/src/utils/accessBackend.ts
+++ webapp/src/utils/accessBackend.ts
@@ -1,5 +1,8 @@
 const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://172.17.0.1:8080'
 
 export function composeBackendUrl(path: string) {
+  if (!BACKEND_BASE_URL) {
+    console.warn('No server URL is set. Using default value.')
+  }
   return BACKEND_BASE_URL + (path.startsWith('/') ? path : `/${path}`)
 }
