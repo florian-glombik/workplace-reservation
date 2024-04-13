109a55c1446b2de06dfa44967f1a9123c732623d
Florian Glombik <florian.glombik@tum.de>
2024-04-13 18:22:39 +0200

    Adding error message to console so mistakes can be easily spotted

--- webapp/src/utils/accessBackend.ts
+++ webapp/src/utils/accessBackend.ts
@@ -1,8 +1,18 @@
 const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://172.17.0.1:8080'
 
 export function composeBackendUrl(path: string) {
-  if (!BACKEND_BASE_URL) {
-    console.warn('No server URL is set. Using default value.')
+  if (!isValidUrl(BACKEND_BASE_URL)) {
+    console.error("The Server URL is not a valid url: " + BACKEND_BASE_URL)
   }
+
   return BACKEND_BASE_URL + (path.startsWith('/') ? path : `/${path}`)
+}
+
+function isValidUrl(url: string): boolean {
+  try {
+    new URL(url);
+    return true;
+  } catch (_) {
+    return false;
+  }
 }
