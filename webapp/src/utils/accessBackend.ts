87b9add66584be08775ca5f9647cfa985f62a5b3
Florian Glombik <florian.glombik@tum.de>
2024-04-13 16:33:46 +0200

    Using environment variable instead of hardcoded value

--- webapp/src/utils/accessBackend.ts
+++ webapp/src/utils/accessBackend.ts
@@ -1,6 +1,4 @@
-const BACKEND_BASE_URL = 'workplace-reservation-backend.florian-glombik.de'
-// process.env.REACT_APP_BACKEND_URL ?? 'http://172.17.0.1:8080'
-// TODO fix environment variable
+const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://172.17.0.1:8080'
 
 export function composeBackendUrl(path: string) {
   return BACKEND_BASE_URL + (path.startsWith('/') ? path : `/${path}`)
