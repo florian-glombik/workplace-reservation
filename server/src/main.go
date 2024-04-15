cb9eef82fe6de0e2ce9fbc210db0d330937bf953
Florian Glombik <florian.glombik@tum.de>
2024-04-15 21:20:08 +0200

    Automatically switch database ip based on development environment

--- server/src/main.go
+++ server/src/main.go
@@ -17,10 +17,11 @@ import (
 )
 
 const (
-	databaseDriver      = "postgres"
-	serverAddress       = "0.0.0.0:8080"
-	EnvDatabasePassword = "POSTGRES_PASSWORD"
-	EnvDatabaseUser     = "POSTGRES_USER"
+	databaseDriver              = "postgres"
+	serverAddress               = "0.0.0.0:8080"
+	EnvDatabasePassword         = "POSTGRES_PASSWORD"
+	EnvDatabaseUser             = "POSTGRES_USER"
+	EnvIsDevelopmentEnvironment = "IS_DEVELOPMENT"
 )
 
 // @title           Workplace Reservation API
@@ -33,15 +34,22 @@ const (
 func main() {
 	databasePassword, isSet := os.LookupEnv(EnvDatabasePassword)
 	databaseUser, isUserSet := os.LookupEnv(EnvDatabaseUser)
+	isDevelopmentEnvironment, _ := os.LookupEnv(EnvIsDevelopmentEnvironment)
 	if !isSet {
 		log.Fatal(fmt.Sprintf("Environment variable '%s' is not defined", EnvDatabasePassword))
 	}
 	if !isUserSet {
 		log.Fatal(fmt.Sprintf("Environment variable '%s' is not defined", EnvDatabaseUser))
 	}
-	// database is not found with 0.0.0.0 on VM
-	databaseSource := fmt.Sprintf("postgresql://%s:%s@database:5432/workplace_reservation?sslmode=disable", databaseUser, databasePassword)
-	//databaseSource := fmt.Sprintf("postgresql://%s:%s@0.0.0.0:5432/workplace_reservation?sslmode=disable", databaseUser, databasePassword)
+
+	databaseIp := ""
+	if isDevelopmentEnvironment == "true" {
+		databaseIp = "0.0.0.0"
+	} else {
+		databaseIp = "database"
+	}
+
+	databaseSource := fmt.Sprintf("postgresql://%s:%s@%s:5432/workplace_reservation?sslmode=disable", databaseUser, databasePassword, databaseIp)
 
 	log.Println("opening database connection ...")
 	databaseConnection, err := sql.Open(databaseDriver, databaseSource)
