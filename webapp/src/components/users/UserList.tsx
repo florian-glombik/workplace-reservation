913eae543d19113087f66220064a270871bd2a34
Florian Glombik <florian.glombik@tum.de>
2024-04-16 13:33:20 +0200

    Adding admin interface to change access rights

---
+++ webapp/src/components/users/UserList.tsx
@@ -0,0 +1,117 @@
+import { Account, useAuth } from '../../utils/AuthProvider'
+import {
+  Box,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  Tooltip,
+  Typography,
+} from '@mui/material'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { getDisplayResponseMessage } from '../../utils/NotificationUtil'
+import { TableHead, TableRow } from '@material-ui/core'
+import { composeServerUrl } from '../../utils/accessServer'
+import LoginIcon from '@mui/icons-material/Login'
+import CheckIcon from '@mui/icons-material/Check'
+import CloseIcon from '@mui/icons-material/Close'
+import BlockIcon from '@mui/icons-material/Block'
+
+export function UserList() {
+  const { jwtToken, user: loggedInUser } = useAuth()
+  const [availableUsers, setAvailableUsers] = useState<Account[]>([])
+
+  const loadUsers = async () => {
+    const requestConfig = {
+      headers: {
+        Authorization: 'Bearer ' + jwtToken,
+      },
+    }
+
+    try {
+      const availableUsersResponse = await axios.get(
+        composeServerUrl('users/all-available'),
+        requestConfig
+      )
+      setAvailableUsers(availableUsersResponse.data)
+    } catch (error) {
+      toast.error('Could not load users: ' + getDisplayResponseMessage(error))
+    }
+  }
+
+  useEffect(() => {
+    loadUsers()
+  }, [])
+
+  const handleAccessRightChange = (
+    user: Account,
+    newAccessGrantedValue: boolean
+  ) => {}
+
+  const noUsersLoaded = availableUsers.length == 0
+  return (
+    <Box>
+      {!noUsersLoaded && (
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Username</TableCell>
+              <TableCell>E-Mail</TableCell>
+              <TableCell>User Role</TableCell>
+              <TableCell>Access granted</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {availableUsers.map((user: Account) => {
+              return (
+                <TableRow key={user.id}>
+                  <TableCell>{user.username.String}</TableCell>
+                  <TableCell>{user.email}</TableCell>
+                  <TableCell>{user.role}</TableCell>
+                  <TableCell>
+                    {user.accessGranted ? <CheckIcon /> : <CloseIcon />}
+                  </TableCell>
+                  <TableCell>
+                    {user.id !== loggedInUser.id && (
+                      <>
+                        {user.accessGranted ? (
+                          <Tooltip title={'Revoke access'}>
+                            <IconButton
+                              onClick={() =>
+                                handleAccessRightChange(user, false)
+                              }
+                              aria-label="revoke access"
+                            >
+                              <BlockIcon />
+                            </IconButton>
+                          </Tooltip>
+                        ) : (
+                          <Tooltip title={'Grant access'}>
+                            <IconButton
+                              onClick={() =>
+                                handleAccessRightChange(user, true)
+                              }
+                              aria-label="grant access"
+                            >
+                              <LoginIcon />
+                            </IconButton>
+                          </Tooltip>
+                        )}
+                      </>
+                    )}
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      )}
+      {noUsersLoaded && (
+        <Typography>No additional users registered yet</Typography>
+      )}
+    </Box>
+  )
+}
--- webapp/src/pages/UsersPage.tsx
+++ webapp/src/pages/UsersPage.tsx
@@ -1,47 +1,15 @@
-import { Box, Divider, Stack, Typography } from '@mui/material'
-import { CreateOrEditOffice } from '../components/offices/CreateOrEditOffice'
-import { OfficeList } from '../components/offices/OfficeList'
+import { Box, Typography } from '@mui/material'
 import { USER_MANAGEMENT_MENU_ENTRY } from '../components/Header'
-import { useState } from 'react'
-import { Account, useAuth } from '../utils/AuthProvider'
-import axios from 'axios'
-import { composeServerUrl } from '../utils/accessServer'
-import { toast } from 'react-toastify'
-import { getDisplayResponseMessage } from '../utils/NotificationUtil'
+import { UserList } from '../components/users/UserList'
 
 export function UsersPage() {
-  const { jwtToken, isAdmin } = useAuth()
-  const [availableUsers, setAvailableUsers] = useState<Account[]>([])
-
-  const loadUsers = async () => {
-    const requestConfig = {
-      headers: {
-        Authorization: 'Bearer ' + jwtToken,
-      },
-    }
-
-    try {
-      const availableUsersResponse = await axios.get(
-        composeServerUrl('users/all-available'),
-        requestConfig
-      )
-      setAvailableUsers(availableUsersResponse.data)
-    } catch (error) {
-      toast.error('Could not load users: ' + getDisplayResponseMessage(error))
-    }
-  }
-
   return (
     <Box sx={{ m: 2 }}>
       <Typography variant={'h4'} sx={{ mb: 4 }}>
         {USER_MANAGEMENT_MENU_ENTRY}
       </Typography>
 
-      <Stack spacing={6}>
-        <CreateOrEditOffice />
-        <Divider />
-        <OfficeList />
-      </Stack>
+      <UserList />
     </Box>
   )
 }
