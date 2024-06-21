7cc5622b0baa7fef26def3ce6ebf18354c8477de
Florian Glombik <florian.glombik@tum.de>
2024-06-21 05:36:32 +0200

    Fixing script for upload

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,31 +3,46 @@
 GITHUB_USERNAME="florian-glombik"
 GITHUB_TOKEN="${GITHUB_TOKEN}"
 REPOSITORY_NAME="workplace-reservation"
-SERVER_IMAGE_NAME="server"
-CLIENT_IMAGE_NAME="client"
 VERSION="v1.0.0"
+DOCKER_REGISTRY="ghcr.io"
+
 
 login_to_docker_registry() {
   echo "Authenticating with GitHub Docker Registry..."
   echo "$GITHUB_TOKEN" | docker login $DOCKER_REGISTRY -u $GITHUB_USERNAME --password-stdin
 }
 
-build_and_push_image() {
-  local IMAGE_NAME=$1
-  local IMAGE_DIR=$2
+build_image() {
+  local SERVICE_NAME=$1
 
-  echo "Building $IMAGE_NAME Docker image..."
-  docker build -t "$IMAGE_NAME":$VERSION $IMAGE_DIR
+  echo "Building $SERVICE_NAME Docker image..."
+  docker-compose build "$SERVICE_NAME"
+}
+
+tag_image() {
+  local SERVICE_NAME=$1
 
   echo "Tagging Docker image..."
-  docker tag "$IMAGE_NAME":$VERSION ghcr.io/$GITHUB_USERNAME/$REPOSITORY_NAME/$IMAGE_NAME:$VERSION
+  docker tag "$SERVICE_NAME" $DOCKER_REGISTRY/$GITHUB_USERNAME/$REPOSITORY_NAME/"$SERVICE_NAME":$VERSION
+}
 
-  echo "Pushing $IMAGE_NAME Docker image to GitHub Container Registry..."
-  docker push ghcr.io/$GITHUB_USERNAME/$REPOSITORY_NAME/$IMAGE_NAME:$VERSION
+push_image() {
+  local SERVICE_NAME=$1
 
-  echo "$IMAGE_NAME Docker image pushed successfully to GitHub Container Registry."
+  echo "Pushing $SERVICE_NAME Docker image to GitHub Container Registry..."
+  docker push $DOCKER_REGISTRY/$GITHUB_USERNAME/$REPOSITORY_NAME/"$SERVICE_NAME":$VERSION
+
+  echo "$SERVICE_NAME Docker image pushed successfully to GitHub Container Registry."
+}
+
+build_and_push_image() {
+    local SERVICE_NAME=$1
+
+    build_image "$SERVICE_NAME"
+    tag_image "$SERVICE_NAME"
+    push_image "$SERVICE_NAME"
 }
 
 login_to_docker_registry
-build_and_push_image $SERVER_IMAGE_NAME ./server
-build_and_push_image $CLIENT_IMAGE_NAME ./webapp
+build_and_push_image server
+build_and_push_image client
